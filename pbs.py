612e8e86c966bc6f29e6f3e8d5f85eea1684160b
Yifan Mai <yifan.mai@sydney.edu.au>
2025-03-03 13:34:25 +1100

    function: write pbs files to run blobby3d

--- pbs.py
+++ pbs.py
@@ -106,3 +106,50 @@ def copy_postpy(data_path,id_list,save_path,save_name):
     for idd in id_list:
         shfile.write('cp '+post_path+' '+data_path+str(idd)+'/post.py\n')
     shfile.close()
+
+
+def pbs_run_b3d(pbs_path,pbs_name,pbs_file_num,id_pix_sorted,data_path,b3d_path):
+    '''
+    pbs_path: path to write pbs file
+    pbs_name: name of pbs file
+    pbs_file_num: to split full sample to how many pbs files
+    id_pix_sorted: 2 column array, first column is id, second column is number 
+                    of pixels. sorted according to pixel number ascending
+    data_path: path that save preblobby3d file (data.txt, var.txt...)
+    b3d_path: path for blobby3d exe
+    
+    
+    
+    '''
+    flag = 0
+    sum_time = 0
+    file_num = 1
+    for flag in range(pbs_file_num):
+        pbsfile = open(pbs_path+pbs_name+"_"+str(file_num)+'.pbs',"w")
+        str_to_write = ''
+        #while True:
+        sum_time += time_calculator(id_pix_sorted[flag][1])
+        str_to_write += 'cd '+data_path+str(int(id_pix_sorted[flag][0]))+'\n'
+        str_to_write += 'export OMP_NUM_THREADS=32\n'
+        str_to_write += '${io}Blobby3D -t 32 -f MODEL_OPTIONS\n'
+            
+        max_pix = id_pix_sorted[flag][1]
+            
+        cpu, mem = cpu_mem(max_pix)
+        pbsfile.write('#!/bin/bash\n')
+        pbsfile.write('#PBS -P blobby3d\n')
+        pbsfile.write('#PBS -N metal_test_con_pa_'+str(file_num)+'\n')
+        pbsfile.write('#PBS -l select=1:ncpus=%d:mem=%dGB\n'%(cpu,mem))
+        if file_num ==7:
+            run_time = int((sum_time+8)*0.7)
+            pbsfile.write('#PBS -l walltime=%d:00:00\n'%(int((sum_time+8)*0.7)))
+        else:
+            run_time = int((sum_time+8)/2)
+            pbsfile.write('#PBS -l walltime=%d:00:00\n'%(int((sum_time+8)/2)))
+        pbsfile.write('#PBS -q defaultQ\n')
+        pbsfile.write('io='+b3d_path+'\n')
+        pbsfile.write(str_to_write)
+        pbsfile.close()
+        print(str(file_num)+': time:'+str(run_time)+' h')
+        sum_time = 0
+        file_num += 1
